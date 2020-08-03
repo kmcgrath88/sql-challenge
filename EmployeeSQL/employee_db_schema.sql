9ec154cbd75af8fe9ee044702efaaf1c34278db8
Kristi McGrath <mcgrathkristi@gmail.com>
2020-08-03 15:10:06 -0400

    Updates

---
+++ EmployeeSQL/employee_db_schema.sql
@@ -0,0 +1,65 @@
+-----Data Engineering -----
+
+--Creating tables, loading data from csv files, and double checking tables loaded properly.
+--Creating departments table.
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+SELECT * FROM departments;
+
+--Creating titles table.
+CREATE TABLE titles(
+	title_id VARCHAR(5) NOT NULL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL
+);
+
+SELECT * FROM titles;
+
+--Creating employees table.
+CREATE TABLE employees(
+	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR(5) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL
+);
+
+SELECT * FROM employees;
+
+--Creating dept_emp table.
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+SELECT * FROM dept_emp;
+
+--Creating dept_manager table.
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY(dept_no, emp_no)
+);
+
+SELECT * FROM dept_manager;
+
+--Creating salaries table.
+CREATE TABLE salaries(
+	emp_no INT NOT NULL PRIMARY KEY,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	salary INT NOT NULL
+);
+
+SELECT * FROM salaries;
+
+
